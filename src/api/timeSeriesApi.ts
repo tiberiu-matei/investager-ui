960f9c4f91cbc524c71aa8951ca1a33d8ce210a0
Tiberiu Matei <matei.tiberiu02@gmail.com>
2021-09-15 20:30:02 +0100

    Add asset chart

--- src/api/timeSeriesApi.ts
+++ src/api/timeSeriesApi.ts
@@ -1,9 +1,9 @@
 import { ApiBaseRoutes } from '../models/app/apiBaseRoutes';
-import { TimeSeriesResponse } from '../models/timeSeries';
+import { TimePointResponse } from '../models/timeSeries';
 import axiosClient from './axiosClient';
 
-export async function Get(key: string): Promise<TimeSeriesResponse> {
-    const response = await axiosClient.get<TimeSeriesResponse>(`${ApiBaseRoutes.timeSeries}/${key}`);
+export async function Get(key: string): Promise<TimePointResponse[]> {
+    const response = await axiosClient.get<TimePointResponse[]>(`${ApiBaseRoutes.timeSeries}/${key}`);
 
     return response.data;
 }
